dfc29d05bcd66a3b16d9f2427e1362ef868d340e
Daniel Gordon <daniel.gordon@here.com>
2015-12-11 11:17:12 -0600

    Initial commit for the octopus merge creator
        - All required scripts have been tested on Git bash for Windows
        - 3 commits are made for each branch
        - Number of branches is specified by the user
        - Number of branches needs to be specified for all three user scripts
        - make_octo_text.sh is a script used by create_octo.sh. The user does not need to call this script on its own
    
    Change-Id: Ie44e30fc869ee023e3b790de84e578c82459763b

---
+++ create_octo.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# script that creates 1 file, with 8 lines
+# commits that file, then creates a branch
+# that branch changes 1 line, and commits - 3x in a row
+# then do the same with a different line, on a different branch
+
+make_octo_txt.sh $1
+
+# Now add and commit this file
+git add octo_test.txt
+git commit -m "Added the octo file with $1 lines"
+
+# Now use a double for loop to create 8 branches and make multiple commits in each branch
+# Create the 'head' branch - the branch all others are created from
+for i in $(eval echo "{1..$1}")
+do
+    git checkout -b octo_branch_$i;
+    for j in {1..3}
+    do
+        sed -i -e "s/line $i _/line $i _ $RANDOM/g" octo_test.txt;
+        git add octo_test.txt;
+        git commit -m "Commit $j for branch $i";
+    done
+    # Since we want all 8 branches based off same branch, checkout master.
+    git checkout master;
+done
+
+# At this point, branch 8 should be checkout out
+# 8 branches created, with 5 commits each
+# print the log graph to see what's going on
+git logsh;
