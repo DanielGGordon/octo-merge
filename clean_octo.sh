d957c6d1e7052f6c6b3a9a5d6e7acbebda95764b
Michael Foss <michael.foss@valtech.com>
2018-10-08 18:17:41 -0400

    Use default of 8 branches when not specified
    Because...you know..."Octo."

--- clean_octo.sh
+++ clean_octo.sh
@@ -2,8 +2,14 @@
 
 # Script to delete all the octo branches
 
+if [[ "$1" != "" ]]; then
+    BRANCHES="$1"
+else
+    BRANCHES="8"
+fi
+
 git checkout master;
-for i in $(eval echo "{1..$1}")
+for i in $(eval echo "{1..$BRANCHES}")
 do
-    git branch -D octo_branch_$i;
+    git branch -D "octo_branch_$i";
 done
